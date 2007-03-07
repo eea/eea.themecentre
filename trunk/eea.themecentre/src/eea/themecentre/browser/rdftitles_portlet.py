41b51be0f3f836853027572815628c5c28f7546e
terletim <terletim@24821446-e7ea-0310-81ac-b189f4507b0b>
2007-03-07 20:12:15 +0000

    Added a portlet for showing what rdf feeds are available. Also enabled themetaggable on links and faqs.

---
+++ trunk/eea.themecentre/src/eea/themecentre/browser/rdftitles_portlet.py
@@ -0,0 +1,28 @@
+from Products.CMFPlone import utils
+from zope.component import getUtility
+from zope.interface import implements
+
+from eea.themecentre.themecentre import getTheme
+from eea.themecentre.themecentre import RDF_THEME_KEY
+from eea.themecentre.browser.interfaces import IRDFTitlesPortlet
+from eea.rdfrepository.interfaces import IRDFRepository
+
+class EEARDFTitlesPortlet(utils.BrowserView):
+    implements(IRDFTitlesPortlet)
+
+    def __init__(self, context, request):
+        super(EEARDFTitlesPortlet, self).__init__(context, request)
+        self._theme = getTheme(context)
+        
+    def feeds(self):
+        context = utils.context(self)
+
+        if self._theme:
+            rdfrepository = getUtility(IRDFRepository)
+            search = { RDF_THEME_KEY: { 'theme': self._theme }}
+            feeds = rdfrepository.getFeedData(search)
+
+        return feeds
+
+    def theme(self):
+        return self._theme
