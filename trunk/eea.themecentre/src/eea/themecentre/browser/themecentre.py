d641534b22eb232cdcaa03614a2df9cdc8664482
terletim <terletim@24821446-e7ea-0310-81ac-b189f4507b0b>
2007-11-29 11:09:42 +0000

    Changed the multimedia section in themecentre to show a listing of all videos instead of having four categories in a square. This fixes #5151.

--- trunk/eea.themecentre/src/eea/themecentre/browser/themecentre.py
+++ trunk/eea.themecentre/src/eea/themecentre/browser/themecentre.py
@@ -1,10 +1,13 @@
+from zope.app.schema.vocabulary import IVocabularyFactory
 from zope.interface import implements, alsoProvides
 from zope.component import getUtility
 from zope.formlib.form import Fields
 from Products.Five.formlib.formbase import EditForm
 from eea.themecentre.interfaces import IThemeCentre, IPossibleThemeCentre
 from eea.themecentre.interfaces import IThemeCentreSchema, IThemeRelation
-from eea.themecentre.themecentre import PromotedToThemeCentreEvent
+from eea.themecentre.themecentre import PromotedToThemeCentreEvent, getTheme
+from eea.mediacentre.mediacentre import MEDIA_SEARCH_KEY
+from eea.mediacentre.mediatypes import MediaTypesVocabularyFactory
 from Products.CMFCore.utils import getToolByName
 
 from eea.mediacentre.interfaces import IMediaCentre
@@ -34,3 +37,23 @@ class ThemeCentreEdit(EditForm):
 
     form_fields = Fields(IThemeCentreSchema, IThemeRelation)
     label = u'Promote theme centre'
+
+class Multimedia(object):
+    """ Provides multimedia information for a themecentre's multimedia section. """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def types(self):
+        from zope.interface import Interface
+        mediacentre = getUtility(IMediaCentre)
+        types = sorted(mediacentre.getMediaTypes())
+        vocab = getUtility(IVocabularyFactory, name="Media types")(self.context)
+        return [{'id':term.value, 'title':term.title} for term in vocab]
+
+    def media_items(self):
+        currentTheme = getTheme(self.context)
+        mediacentre = getUtility(IMediaCentre)
+        search = { MEDIA_SEARCH_KEY: { 'theme': currentTheme }}
+        return [file['object'] for file in mediacentre.getMedia(search=search)]
