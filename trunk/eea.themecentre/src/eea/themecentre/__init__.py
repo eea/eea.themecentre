adfc08abfc4af9f75b79a2c8348b9499dcbda027
vincisas <vincisas@24821446-e7ea-0310-81ac-b189f4507b0b>
2008-10-28 07:55:36 +0000

    forgotten commit, a message factory for eea.translations

--- trunk/eea.themecentre/src/eea/themecentre/__init__.py
+++ trunk/eea.themecentre/src/eea/themecentre/__init__.py
@@ -2,4 +2,7 @@
 from ZPublisher import HTTPRequest
 from zope.publisher.base import DebugFlags
 
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('eea.translations')
+
 HTTPRequest.HTTPRequest.debug = DebugFlags()
