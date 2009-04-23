a87eebef165c0dc5d5aed438439b748e7aae14da
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-04-23 15:33:39 +0000

    Added new internal promotions on themepage

--- trunk/eea.themecentre/src/eea/themecentre/browser/portlets/promotion.py
+++ trunk/eea.themecentre/src/eea/themecentre/browser/portlets/promotion.py
@@ -1,5 +1,6 @@
 from eea.themecentre.themecentre import getTheme, getThemeCentre
 from Products.CMFCore.utils import getToolByName
+from eea.promotion.interfaces import IPromotion
 
 class ThemeCentreMenuPromotion(object):
     """ Return the promotion to show as part of navigation. Most of the time an
@@ -12,15 +13,15 @@ class ThemeCentreMenuPromotion(object):
     def promotions(self, section=None):
         currentTheme = getTheme(self.context)
         catalog = getToolByName(self.context, 'portal_catalog')
+        promotions = []
+
+        # External promotions
         query = { 'portal_type' : 'Promotion',
                   'review_state' : 'published',
                   'getThemes' : currentTheme }
         if section is not None:
             query['navSection'] = section
-        
         result = catalog.searchResults( query )
-        promotions = []
-
         for t in result:
             if section is not None or (section is None and t.navSection in [None, 'default']):
                 promotions.append( {'id' : t.getId,
@@ -29,6 +30,26 @@ class ThemeCentreMenuPromotion(object):
                                     'url' : t.getUrl,
                                     'style' : 'display: none;',
                                     'image' : t.getURL() + '/image' } )
+
+        # Internal promotions
+        query = {'object_provides': 'eea.promotion.interfaces.IPromoted',
+                 'review_state': 'published'}
+        result = catalog.searchResults(query)
+        for t in result:
+            promo = IPromotion(t.getObject())
+            if not promo.display_on_themepage:
+                continue
+            if not currentTheme in promo.themes:
+                continue
+            if (section != None) and (section != promo.themepage_section):
+                continue
+            promotions.append( {'id' : t.getId,
+                                'Description' : t.Description,
+                                'Title' : t.Title,
+                                'url' : t.getUrl,
+                                'style' : 'display: none;',
+                                'image' : t.getURL() + '/image' } )
+
         if promotions:
             promotions[0]['style'] = 'display: block'
         return promotions
