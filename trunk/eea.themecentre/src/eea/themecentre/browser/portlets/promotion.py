aca1eacde1c89579fa1580c11e016dacc65fe295
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-06-11 12:11:06 +0000

    Use effectiveRange in promotion queries. Solves second point in #2372

--- trunk/eea.themecentre/src/eea/themecentre/browser/portlets/promotion.py
+++ trunk/eea.themecentre/src/eea/themecentre/browser/portlets/promotion.py
@@ -2,6 +2,7 @@ from zope.component import getMultiAdapter
 from eea.themecentre.themecentre import getTheme, getThemeCentre
 from Products.CMFCore.utils import getToolByName
 from eea.promotion.interfaces import IPromotion
+from DateTime.DateTime import DateTime
 
 class ThemeCentreMenuPromotion(object):
     """ Return the promotion to show as part of navigation. Most of the time an
@@ -15,18 +16,21 @@ class ThemeCentreMenuPromotion(object):
         currentTheme = getTheme(self.context)
         catalog = getToolByName(self.context, 'portal_catalog')
         promotions = []
+        now = DateTime()
 
         # External promotions
         query = { 'object_provides': 'Products.EEAContentTypes.content.interfaces.IExternalPromotion',
                   'review_state': 'published',
-                  'getThemes': currentTheme }
+                  'getThemes': currentTheme,
+                  'effectiveRange' : now}
         if section is not None:
             query['navSection'] = section
         result1 = catalog.searchResults( query )
 
         # Internal promotions
         query = {'object_provides': 'eea.promotion.interfaces.IPromoted',
-                 'review_state': 'published'}
+                 'review_state': 'published',
+                 'effectiveRange' : now}
         result2 = catalog.searchResults( query )
 
         for t in result1 + result2:
