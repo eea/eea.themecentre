d90e1260491ad46a1fe3f07dd3a5a768bc7a2603
vincisas <vincisas@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-12-07 06:50:58 +0000

    back to development

--- trunk/eea.themecentre/branches/cop15-redesign/setup.py
+++ trunk/eea.themecentre/branches/cop15-redesign/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='eea.themecentre',
-      version = '0.2',
+      version = '0.3 svn',
       url = 'http://svn.eionet.europa.eu/repositories/Zope',
       description = 'ThemeCentre',
       author = 'Tim Terlegard, Sasha Vincic',
