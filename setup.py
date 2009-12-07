826c5109ac28db1e460a3c20b78e03332a122730
Sasha Vinčić <vincic@users.noreply.github.com>
2009-12-07 06:50:18 +0000

    Tagged eea.themecentre 0.2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='eea.themecentre',
-      version = '0.1',
+      version = '0.2',
       url = 'http://svn.eionet.europa.eu/repositories/Zope',
       description = 'ThemeCentre',
       author = 'Tim Terlegard, Sasha Vincic',
