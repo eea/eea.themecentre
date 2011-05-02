db514e7eb8008f4d1ecb20ee058c9044dae505a7
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-02 15:50:17 +0000

    Fixed ea.themecentre profile registartion, refs #8241

--- trunk/eea.themecentre/branches/plone4/src/eea/themecentre/__init__.py
+++ trunk/eea.themecentre/branches/plone4/src/eea/themecentre/__init__.py
@@ -1,11 +1,4 @@
 """ eea.themecentre package """
 
-#TODO: clean up below, plone4
-## monkey patch of Zope
-#from ZPublisher import HTTPRequest
-#from zope.publisher.base import DebugFlags
-
-#HTTPRequest.HTTPRequest.debug = DebugFlags()
-
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('eea.translations')
