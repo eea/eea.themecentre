9c0eb85fa60cc96d416fef68a80ee5f9bb068cc9
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-03 08:17:59 +0000

    Added eea.themecentre import steps, refs #8241

---
+++ trunk/eea.themecentre/branches/plone4/src/eea/themecentre/setuphandlers.py
@@ -0,0 +1,43 @@
+""" EEA Theme Centre import steps
+"""
+from Products.CMFCore.utils import getToolByName
+
+def setupMergedThemesVocabulary(context):
+    site = context.getSite()
+    vocab = getToolByName(site, 'portal_vocabularies')
+
+    if not getattr(vocab, 'themesmerged', None):
+        vocab.invokeFactory('TreeVocabulary', id='themesmerged')
+    mergedthemes = vocab.getVocabularyByName('themesmerged')
+
+    themes = {'air_quality': 'air',
+              'acidification': 'air',
+              'information': 'reporting',
+              'management': 'policy',
+              'nature': 'biodiversity',
+              'ozone': 'climate',
+              'reporting': 'policy'}
+
+    for theme, mergedwith in themes.items():
+        if not hasattr(mergedthemes, theme):
+            tId = theme
+            tId = mergedthemes.invokeFactory('TreeVocabularyTerm',
+                                             id=tId)
+            t = mergedthemes[tId]
+            t.setTitle(theme)
+            mId = mergedwith
+            mId = mergedthemes[tId].invokeFactory('TreeVocabularyTerm',
+                                                  id=mId)
+            t[mId].setTitle(mergedwith)
+
+def setupVarious(context):
+    if context.readDataFile('eea.themecentre.txt') is None:
+        return
+
+    logger = context.getLogger('eea.themecentre')
+
+    setupMergedThemesVocabulary(context)
+    logger.info("EEA Theme Centre: merged themes vocabulary added")
+
+    logger.info("EEA Theme Centre: done setting import steps")
+    return
