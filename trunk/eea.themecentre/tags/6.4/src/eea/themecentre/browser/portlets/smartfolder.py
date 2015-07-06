28c455eb502235b292621b695e6b12748c047b18
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-07-06 12:17:09 +0000

    Tagged eea.themecentre 6.4.

---
+++ trunk/eea.themecentre/tags/6.4/src/eea/themecentre/browser/portlets/smartfolder.py
@@ -0,0 +1,46 @@
+""" Smart folder
+"""
+from Products.EEAContentTypes.browser import smartfolder
+from eea.themecentre.themecentre import getThemeCentre
+from eea.themecentre import eeaMessageFactory as _
+
+class SmartFolderPortlets(smartfolder.SmartFolderPortlets):
+    """ Smart Folder Portlets
+    """
+
+    def __call__(self):
+        themecentre = getThemeCentre(self.context)
+        if themecentre is None:
+            return []
+        else:
+            return self.portlets(themecentre)
+
+    def _sort_key(self, topic):
+        """ Sort key of topic
+        """
+        tid = topic.getId()
+
+        if tid == 'highlights_topic':
+            return "1"
+        elif tid == 'events_topic':
+            # we want events at the end
+            return "zzz"
+        else:
+            # all topics don't need to be hardcoded, for the rest we
+            # rely on the topic id for sorting
+            return tid
+
+    def _title(self, topic):
+        """ Returns harcoded titles if matched criteria otherwise default title
+        """
+        obj = self._parent_or_topic(topic)
+        obj_id = obj.getId()
+        obj_title = obj.Title()
+
+        if obj_id == 'events':
+            title = _(u'Upcoming events')
+        elif obj_id == 'faq':
+            title = _(u'Latest FAQ')
+        else:
+            title = obj_title
+        return title
